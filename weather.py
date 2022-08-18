071e0e345c9e5bd115c1a94356d53970b440eb3b
JHuangg <johnh.2002@hotmail.com>
2022-08-17 22:10:01 -0400

    Updates to weather recommendations

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 import discord
+import json
 
 # Function to convert kelvins to celsius
 def convertTemp(kelvin):
@@ -11,3 +12,26 @@ def parseData(data):
     currentTemp = int(convertTemp(kelvinTemp))
     currentFeeling = int(convertTemp(kelvinFeelingTemp))
     return (f"The current weather is {currentTemp}°C and the current feeling is {currentFeeling}°C")
+
+def appropriateClothing(weather, weatherDescription):
+    clothingRecommendations = []
+    with open('closet.json') as inv:
+        closet_data = json.load(inv)
+    
+    if weather <= 0 or weatherDescription == 'snowing':
+        for item in closet_data:
+            if item['type'] == "Jacket" or item['type'] == "Boots":
+                clothingRecommendations.append(item)
+
+    elif 0 < weather <= 21:
+        for item in closet_data:
+            if item['type'] == "Sweater":
+                clothingRecommendations.append(item)
+    else:
+        for item in closet_data:
+            if item['type'] == "Shirt":
+                clothingRecommendations.append(item)
+
+    return clothingRecommendations
+
+print(appropriateClothing(-42, "nothing"))
